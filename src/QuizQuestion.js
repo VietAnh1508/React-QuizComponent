ca6b9772079d2f51d409796967092c2d53ecd903
Thien Viet Anh Tong <toah@Thiens-MacBook-Pro.local>
2022-06-14 14:37:31 +0700

    display an error message based on state

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -4,10 +4,21 @@ import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            incorrectAnswer: false
+        }
+    }
+
     handleClick(buttonText) {
         if (buttonText === this.props.quiz_question.answer) {
             this.props.showNextQuestionHandler();
         }
+
+        this.setState({
+            incorrectAnswer: buttonText !== this.props.quiz_question.answer
+        });
     }
 
     render() {
@@ -30,6 +41,7 @@ class QuizQuestion extends Component {
                         }
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>Sorry, that's not right</p> : null}
             </main >
         )
     }
